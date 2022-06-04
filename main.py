35980d453721bf5536af645457dd5fc8cd05b546
Koen Reintsema <446450@student.fontys.nl>
2022-06-04 18:41:36 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+import cassiopeia as cass
+
+cass.set_riot_api_key("RGAPI-6bc918ac-5018-4933-8e30-6b9df80e2569")
+
+summoner = cass.get_summoner(name="KoenVit", region="EUW")
+print("{name} is a level {level} summoner on the {region} server.".format(name=summoner.name,
+                                                                          level=summoner.level,
+                                                                          region=summoner.region))
+
+champions = cass.get_champions(region="NA")
+random_champion = random.choice(champions)
+print("He enjoys playing champions such as {name}.".format(name=random_champion.name))
+
+challenger_league = cass.get_challenger_league(queue=cass.Queue.ranked_solo_fives)
+best_na = challenger_league[0].summoner
+print("He's not as good as {name} at League, but probably a better python programmer!".format(name=best_na.name))
