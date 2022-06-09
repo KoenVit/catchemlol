508439eec57d70c01be5c4e89d803a805a10cacc
Koen Koedam <koedamkoen@gmail.com>
2022-06-09 18:48:57 +0200

    commit message

---
+++ Catchemlol/main.py
@@ -0,0 +1,15 @@
+from ast import Set
+import challenges as challenge
+
+def main():
+    challenge.SetAPI()
+
+    print("\nWelcome to Catch 'em LoL.")
+    regionInput = (str)(input('Type in your region: '))
+    nameInput = (str)(input('Type in your summoner name: '))
+    
+    challenge.CatchEmAll(nameInput, regionInput)
+    input("Press enter to quit")
+
+if __name__ == "__main__":
+    main()
---
+++ Catchemlol/requirements.py
@@ -0,0 +1,12 @@
+from enum import IntEnum
+
+class CatchEmAllRequirement(IntEnum):
+    IRON = 100
+    BRONZE = 500
+    SILVER = 1000
+    GOLD = 5000
+    PLATINUM = 10000
+    DIAMOND = 50000
+    MASTER = 100000
+    GRANDMASTER = 107500
+    CHALLENGER = 115000
