33151a115de6ce24aa05af4b07ce7b7aa6ec942e
Koen Koedam <koedamkoen@gmail.com>
2022-06-09 15:16:13 +0200

    Changed to constants

--- Catchemlol/catchemlol.py
+++ Catchemlol/catchemlol.py
@@ -1,23 +1,29 @@
 import cassiopeia as cass
 from rank import Rank
+from constants import apiKey, championsChallenge
 
 def PrintList(list, rank, region):
     sumPointsWithout = 0
-    championsChallenge = 150
     championsThreshold = len(cass.get_champions(region)) - championsChallenge
-    print("These champions are not yet at " + str(rank) + " mastery points: ")
+    if(len(list) == 0):
+        print("You have every single champion on " + str(rank) + " mastery points. Amazing!")
+    else:
+        print("These champions are not yet at " + str(rank) + " mastery points: ")
+        for cm in list:
+            if(len(list) - list.index(cm) > championsThreshold):
+                print("\033[1;33;40m" + cm.champion.name, cm.points)
+                sumPointsWithout += cm.points
+            else:
+                print("\033[1;31;40m" + cm.champion.name, cm.points)
+        remainingMasteryWithout = ((len(list) - championsThreshold) * rank) - sumPointsWithout
+        championsToDo = str(len(list) - championsThreshold)
 
-    for cm in list:
-        if(len(list) - list.index(cm) > championsThreshold):
-            print("\033[1;33;40m" + cm.champion.name, cm.points)
-            sumPointsWithout += cm.points
+        if(remainingMasteryWithout > 0):
+            print("\033[1;37;40mThis means that you still need a total of", str(remainingMasteryWithout), "mastery points over", championsToDo, "champions.")
         else:
-            print("\033[1;31;40m" + cm.champion.name, cm.points)
-    remainingMasteryWithout = ((len(list) - championsThreshold) * rank) - sumPointsWithout
-    print("\033[1;37;40mThis means that you still need a total of", str(remainingMasteryWithout), "mastery points over", str(len(list) - championsThreshold), "champions.")
+            print("\033[1;37;40mCongratulations! You have at least " + str(championsChallenge) + " champions on " + str(rank) + " mastery points.")
 
 def main():
-    from apikey import apiKey
     cass.set_riot_api_key(apiKey)
 
     print("\nWelcome to Catch 'em LoL.")
@@ -27,8 +33,7 @@ def main():
 
     masteries = cass.get_champion_masteries(nameInput, regionInput).filter(lambda cm: cm.points < rankInput)
     PrintList(masteries, rankInput, regionInput)
-
     input("Press enter to quit")
-    
+
 if __name__ == "__main__":
     main()
