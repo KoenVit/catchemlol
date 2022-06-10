5cddf2f89129f537a387b0e8f9bba800c9934c78
Koen Koedam <koedamkoen@gmail.com>
2022-06-10 09:25:47 +0200

    You're cute

--- Catchemlol/challenges.py
+++ Catchemlol/challenges.py
@@ -4,19 +4,18 @@ from constants import apiKey
 def SetAPI():
     cass.set_riot_api_key(apiKey)
 
-def CatchEmAll(name, region):
+def CatchEmAll(name, region, rank):
     from requirements import CatchEmAllRequirement
-    from constants import catchEmAllChallenge
+    from requirements import catchEmAllChallenge
     sumPointsWithout = 0
 
-    rankInput = CatchEmAllRequirement[(input('Type in what rank of Catch em all you want to see: ').upper())].value     # Ask for user input for rank
-    masteries = cass.get_champion_masteries(name, region).filter(lambda cm: cm.points < rankInput)                      # List with correct filter depending on input                                                                                   
+    masteries = cass.get_champion_masteries(name, region).filter(lambda cm: cm.points < rank)                           # List with correct filter depending on input                                                                                   
     championsThreshold = len(cass.get_champions(region)) - catchEmAllChallenge                                          # See threshold for challenge
      
     if(len(masteries) == 0):
-        print("You have every single champion on " + str(rankInput) + " mastery points. Amazing!")
+        print("You have every single champion on " + str(rank) + " mastery points. Amazing!")
     else:
-        print("These champions are not yet at " + str(rankInput) + " mastery points: ")
+        print("These champions are not yet at " + str(rank) + " mastery points: ")
         from constants import TextColor as color
         for cm in masteries:
             if(len(masteries) - masteries.index(cm) > championsThreshold):
@@ -25,7 +24,7 @@ def CatchEmAll(name, region):
             else:
                 print(color.RED + cm.champion.name, cm.points)
 
-        remainingMasteryWithout = ((len(masteries) - championsThreshold) * rankInput) - sumPointsWithout
+        remainingMasteryWithout = ((len(masteries) - championsThreshold) * rank) - sumPointsWithout
         championsToDo = str(len(masteries) - championsThreshold)
 
         if(remainingMasteryWithout > 0):
@@ -33,4 +32,10 @@ def CatchEmAll(name, region):
              "mastery points over", championsToDo, "champions.")
         else:
             print(color.WHITE + "Congratulations! You have at least " + str(catchEmAllChallenge) + 
-            " champions on " + str(rankInput) + " mastery points.")
+            " champions on " + str(rank) + " mastery points.")
+
+def OneTrick(name, region, rank):
+    masteries = max(cass.get_champion_masteries(name, region))                               # List with correct filter depending on input
+    print("Your current highest mastery champion is " + masteries.champion.name + " with " + masteries.champion.points + " mastery points.")
+    masteryNeeded = rank - masteries.champion.name
+    print("You need " + masteryNeeded + " more mastery points to get to " + rank + " mastery points!")
--- Catchemlol/main.py
+++ Catchemlol/main.py
@@ -2,13 +2,18 @@ from ast import Set
 import challenges as challenge
 
 def main():
+    #from requirements import CatchEmAllRequirement
+    from requirements import OneTrickRequirement
     challenge.SetAPI()
 
     print("\nWelcome to Catch 'em LoL.")
     regionInput = (str)(input('Type in your region: '))
     nameInput = (str)(input('Type in your summoner name: '))
+    #rankInput = CatchEmAllRequirement[(input('Type in what rank of Catch em all you want to see: ').upper())].value     # Ask for user input for rank
+    rankInput = OneTrickRequirement[(input('Type in what rank of Catch em all you want to see: ').upper())].value     # Ask for user input for rank
     
-    challenge.CatchEmAll(nameInput, regionInput)
+    #challenge.CatchEmAll(nameInput, regionInput, rankInput)
+    challenge.OneTrick(nameInput, regionInput, rankInput)
     input("Press enter to quit")
 
 if __name__ == "__main__":
